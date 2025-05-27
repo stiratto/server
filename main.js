d99e00bae76270379a8809f9aabdbe1d79e27dd1
stiratto <moranmoralesj4@gmail.com>
2025-05-26 20:24:36 -0500

    added get endpoint for testing

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ app.post('/receive-code', (req, res) => {
   console.log("Received code")
 })
 
+app.get('/greeting', (req, res) => {
+  res.send("Hola perro hp")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
