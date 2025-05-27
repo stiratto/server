0e7c27f524b05486a92e8e07b1b6e4caa9ff3b37
stiratto <moranmoralesj4@gmail.com>
2025-05-26 20:39:49 -0500

    asd

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
+
+
+app.use(cors())
+app.use(express.json())
 
 app.post('/receive-code', (req, res) => {
   console.log("Received code")
